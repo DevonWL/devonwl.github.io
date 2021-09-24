503772e06e64429dbd357b7704ee688bfa8db206
DevonWL <d.william.leigh@gmail.com>
2021-09-24 09:02:29 -0300

    better nav menu pullout

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 
+
+function openNav(){
+    document.getElementById("nav-bar").style.width = '10%';
+}
+
+function closeNav(){
+    document.getElementById("nav-bar").style.width = '0%';
+}
+
 /* change menuBtn to menuBtn.open */
 const menuBtn = document.querySelector('.menu-btn');
 const navBar = document.querySelector('.nav-bar');
@@ -6,16 +15,17 @@ const mainContent = document.querySelector('.main-content');
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
     if (!menuOpen) {
-        menuBtn.classList.add('open');
         menuOpen = true;
-        navBar.classList.add('open')
-        mainContent.classList.add('open');
+        menuBtn.classList.add('open');
+        mainContent.classList.add('open'); 
+        openNav();
 
     } else {
-        menuBtn.classList.remove('open');
         menuOpen = false;
+        menuBtn.classList.remove('open');
         navBar.classList.remove('open');
         mainContent.classList.remove('open');
+        closeNav();
     }
 })
 
